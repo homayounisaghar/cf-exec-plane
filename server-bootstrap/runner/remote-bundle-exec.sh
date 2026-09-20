737a2a39f2b43539e4816d814dae13bbfb0ec842
homayounisaghar <homayounisaghar@gmail.com>
2026-09-20 18:48:04 +0330

    pass bounded Onshape browser-open target to VPS

--- server-bootstrap/runner/remote-bundle-exec.sh
+++ server-bootstrap/runner/remote-bundle-exec.sh
@@ -32,6 +32,9 @@ remote_env=("VPS_SSH_PORT=$port")
 [[ -n "${CF_NEW_SSH_PUBLIC_KEY:-}" ]] && remote_env+=("CF_NEW_SSH_PUBLIC_KEY=$CF_NEW_SSH_PUBLIC_KEY")
 [[ -n "${CF_DEPLOY_SIGNING_PUBLIC_KEY:-}" ]] && remote_env+=("CF_DEPLOY_SIGNING_PUBLIC_KEY=$CF_DEPLOY_SIGNING_PUBLIC_KEY")
 [[ -n "${CF_BACKUP_MODE:-}" ]] && remote_env+=("CF_BACKUP_MODE=$CF_BACKUP_MODE")
+[[ -n "${CF_ONSHAPE_BROWSER_OPEN_DOCUMENT_ID:-}" ]] && remote_env+=("CF_ONSHAPE_BROWSER_OPEN_DOCUMENT_ID=$CF_ONSHAPE_BROWSER_OPEN_DOCUMENT_ID")
+[[ -n "${CF_ONSHAPE_BROWSER_OPEN_WORKSPACE_ID:-}" ]] && remote_env+=("CF_ONSHAPE_BROWSER_OPEN_WORKSPACE_ID=$CF_ONSHAPE_BROWSER_OPEN_WORKSPACE_ID")
+[[ -n "${CF_ONSHAPE_BROWSER_OPEN_ELEMENT_ID:-}" ]] && remote_env+=("CF_ONSHAPE_BROWSER_OPEN_ELEMENT_ID=$CF_ONSHAPE_BROWSER_OPEN_ELEMENT_ID")
 remote_cmd='set -euo pipefail; d=$(mktemp -d /root/.cf-bootstrap.XXXXXX); trap '\''rm -rf "$d"'\'' EXIT; tar -xzf - -C "$d"; cd "$d"; env'
 for kv in "${remote_env[@]}"; do
   printf -v q '%q' "$kv"
